a5b108de82d9c71ad708c5288c4043e304d01696
Jason Wagner <jason@nialscorva.net>
2014-09-15 15:27:47 -0400

    Added validation tests for applications

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,20 @@ module.exports = function(grunt) {
           sample: {
               src: ['mock/**/*.json']
           }
-      }  
+      },
+      tv4: {
+          applicationSchema: {
+              options: { 
+                  root: grunt.file.readJSON('schema/vnd.ozp-application-v1+json.json'),
+                  multi: true,
+                  schemas: {
+                      "http://ozoneplatform.org/jsonschema/hal-schema.json" : grunt.file.readJSON('schema/hal-schema.json'),
+                      "http://ozoneplatform.org/jsonschema/ozp-iwc-base.json" : grunt.file.readJSON('schema/ozp-iwc-base.json')
+                  }
+              },
+              src: 'mock/api/application/v1/*/*.json'
+          }
+      }
     };
     // load all grunt tasks matching the `grunt-*` pattern
     require('load-grunt-tasks')(grunt);
@@ -13,6 +26,6 @@ module.exports = function(grunt) {
     grunt.initConfig(config);
 
     // Default task(s).
-    grunt.registerTask('default', ['jsonlint']);
+    grunt.registerTask('default', ['jsonlint','tv4']);
 
 };
